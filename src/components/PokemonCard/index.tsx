e78157da4adf833c62bea7a58b0781689ac39c66
VictorVelozo <victor.velozo@outlook.com>
2023-01-24 23:02:07 -0300

    updated react version, changed styles and added lazy image loader

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,71 +1,87 @@
 import api from "@services/api";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { ListItem } from "./styles";
-import {MdCatchingPokemon} from 'react-icons/md'
-interface PokemonCardProps{
-    url: string,
-    index: number,
-    onOpenPokemonDetailsModal: (url: string) => void;
+import { MdCatchingPokemon } from "react-icons/md";
+import { motion } from "framer-motion";
+interface PokemonCardProps {
+  url: string;
+  index: number;
+  onOpenPokemonDetailsModal: (url: string) => void;
 }
-export interface PokemonData{
-    name: string,
-    sprites: {
-        other: {
-            'official-artwork': {
-                front_default: string;
-            }
-        }
-    },
-    types: {
-        type: {
-            name: string,
-            url: string,
-        }
-    }[],
-    species: {
-        url: string
-    },
-    weight: number,
-    height: number,
+export interface PokemonData {
+  name: string;
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
+  types: {
+    type: {
+      name: string;
+      url: string;
+    };
+  }[];
+  species: {
+    url: string;
+  };
+  weight: number;
+  height: number;
 }
 
-export function PokemonCard(props: PokemonCardProps){
-    const [isLoading, setIsLoading] = useState(true);
-    const [pokemonData, setPokemonData] = useState<PokemonData>({} as PokemonData);
+export function PokemonCard(props: PokemonCardProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState<PokemonData>(
+    {} as PokemonData
+  );
 
-    useEffect(() => {
-        setIsLoading(true)
-        const loadPokemon = async () => {
-            const res = await api.get(props.url)
-            setPokemonData(res.data)
-        }
-        loadPokemon()
-        setIsLoading(false)
-    }, [])
+  useEffect(() => {
+    setIsLoading(true);
+    const loadPokemon = async () => {
+      const res = await api.get(props.url);
+      setPokemonData(res.data);
+    };
+    loadPokemon();
+    setTimeout(() => setIsLoading(false), 800); //0.8s waiting images to load and display
+  }, []);
 
-    return (
-        <ListItem
-            transition={{
-                type: 'spring',
-                damping: 100,
-            }}
+  return (
+    <ListItem
+      transition={{
+        type: "spring",
+        damping: 100,
+      }}
+    >
+      <button
+        type="button"
+        onClick={() => props.onOpenPokemonDetailsModal(props.url)}
+      >
+        <motion.img
+          style={{ display: isLoading ? "none" : "block" }}
+          src={pokemonData?.sprites?.other["official-artwork"]?.front_default}
+          animate={{ opacity: isLoading ? 0 : 1 }}
+          initial={{ opacity: 0 }}
+        />
+        <div
+          className="loader"
+          style={{ display: isLoading ? "block" : "none" }}
         >
-            {isLoading && <div className="loader">
-                    <MdCatchingPokemon />
-                </div>
-            }
-            <button type='button' onClick={()=>props.onOpenPokemonDetailsModal(props.url)}>
-                <img src={pokemonData?.sprites?.other["official-artwork"]?.front_default}/>
-            </button>
-            <strong className="index">Nº{String(props.index+1).padStart(3, '0')}</strong>
-            <strong className="name">{pokemonData?.name}</strong>
-            <span>
-                {
-                    pokemonData && pokemonData?.types?.map(({ type}, index) => (<p key={index}className={type.name}>{type.name}</p>))
-                }
-            </span>
-
-
-        </ListItem>
-    )
+          <MdCatchingPokemon />
+        </div>
+      </button>
+      <strong className="index">
+        Nº{String(props.index + 1).padStart(3, "0")}
+      </strong>
+      <strong className="name">{pokemonData?.name}</strong>
+      <span>
+        {pokemonData &&
+          pokemonData?.types?.map(({ type }, index) => (
+            <p key={index} className={type.name}>
+              {type.name}
+            </p>
+          ))}
+      </span>
+    </ListItem>
+  );
 }
--- src/components/PokemonCard/styles.ts
+++ src/components/PokemonCard/styles.ts
@@ -2,149 +2,149 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const ListItem = styled(motion.li).attrs({
-    animate: {
-        opacity: [0,1,1],
-        y: [20,-20,0]
-    }
+  animate: {
+    opacity: [0, 1, 1],
+    y: [20, -20, 0],
+  },
 })`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  gap: 4px;
+  border-radius: 0.5rem;
+  box-shadow: 0px 0.25rem 1rem rgba(0, 0, 0, 0.1);
+  padding: 1rem;
+  position: relative;
+
+  button {
+    background: none;
+    border: 0;
+    cursor: pointer;
+  }
+
+  .loader {
+    width: 14rem;
+    height: 14rem;
+  }
+  svg {
+    width: 100%;
+    height: 100%;
+    color: #eee;
+    animation: spin 2s linear infinite;
     display: flex;
-    flex-direction: column;
-    align-items: center; 
-    background-color: white;
-    border-radius: 0.5rem;
-    box-shadow: 0px 0.25rem 1rem rgba(0,0,0,0.1);
-    padding: 1rem;
+    align-items: center;
+    justify-content: center;
+  }
 
-    button {
-        background: none;
-        border: 0;
-        cursor: pointer;
+  @keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+
+  strong {
+    &.name {
+      width: 100%;
+      text-align: left;
+      font-size: 2rem;
+      line-height: 2rem;
+      font-weight: bold;
+      text-transform: capitalize;
+      color: #333;
+      font-family: "Teko";
     }
 
-    .loader{
-        width: 100%;
-        height: 100%;
-        background-color: white;
-        position: absolute;
-        top: 0;
-        left: 0;
+    &.index {
+      width: 100%;
+      text-align: left;
+      font-family: "Teko";
+      font-size: 1.25rem;
+      line-height: 1.25rem;
+      color: #ccc;
     }
-    svg{
-        width: 100%;
-        height: 100%;
-        color: #eee;
-        animation: spin 2s linear infinite;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+  }
+  img {
+    width: 14rem;
+    background-color: #eee;
+    border-radius: 0.25rem;
+    padding: 0.5rem;
+    min-height: 10rem;
+  }
+
+  span {
+    width: 100%;
+    display: flex;
+    gap: 0.25rem;
+    p {
+      padding: 0.25rem 1rem;
+      border-radius: 0.25rem;
+      font-family: "Teko";
+      font-weight: bold;
+      line-height: 1rem;
+      color: white;
+      text-transform: uppercase;
+      &.grass {
+        background-color: #9bcc50;
+      }
+      &.ghost {
+        background-color: #7b62a3;
+      }
+      &.poison {
+        background-color: #b97fc9;
+      }
+      &.fire {
+        background-color: #fd7d24;
+      }
+      &.water {
+        background-color: #4592c4;
+      }
+      &.bug {
+        background-color: #729f3f;
+      }
+      &.flying {
+        background: linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%);
+      }
+      &.normal {
+        background-color: #a4acaf;
+      }
+      &.electric {
+        background-color: #eed535;
+      }
+      &.ground {
+        background: linear-gradient(180deg, #f7de3f 50%, #ab9842 50%);
+      }
+      &.fairy {
+        background-color: #fdb9e9;
+      }
+      &.poison {
+        background-color: #a040a0;
+      }
+      &.fighting {
+        background-color: #d56723;
+      }
+      &.psychic {
+        background-color: #f366b9;
+      }
+      &.rock {
+        background-color: #a38c21;
+      }
+      &.steel {
+        background-color: #9eb7b8;
+      }
+      &.ice {
+        background-color: #51c4e7;
+      }
+      &.dragon {
+        background: linear-gradient(180deg, #53a4cf 50%, #f16e57 50%);
+      }
+      &.dark {
+        background-color: #707070;
+      }
     }
-
-    @keyframes spin {
-        0% { -webkit-transform: rotate(0deg); }
-        100% { -webkit-transform: rotate(360deg); }
-    }
-
-
-    strong {
-        &.name{
-            width: 100%;
-            text-align: left;
-            font-size: 2rem;
-            line-height: 2rem;
-            font-weight: bold;
-            text-transform: capitalize;
-            color: #333;
-            font-family: 'Teko';
-        }
-
-        &.index{
-            width: 100%;
-            text-align: left;
-            font-family: 'Teko';
-            font-size: 1.25rem;
-            line-height: 1.25rem;
-            color: #ccc;
-        }
-
-    }
-    img{
-        width: 14rem;
-        background-color: #eee;
-        border-radius: 0.25rem;
-        padding: 0.5rem;
-        min-height: 10rem;
-    }
-    
-    span {
-        width: 100%;
-        display: flex;
-        gap: 0.25rem;
-        p{
-            padding: 0.25rem 1rem;
-            border-radius: 0.25rem;
-            font-family: 'Teko';
-            font-weight: bold;
-            line-height: 1rem;
-            color: white;
-            text-transform: uppercase;
-            &.grass{
-                background-color: #9bcc50;
-            }
-            &.ghost{
-                background-color: #7b62a3;
-            }
-            &.poison{
-                background-color: #b97fc9;
-            }
-            &.fire{
-                background-color: #fd7d24;
-            }
-            &.water{
-                background-color: #4592c4;
-            }
-            &.bug{
-                background-color: #729f3f;
-            }
-            &.flying{
-                background: linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%);
-            }
-            &.normal{
-                background-color: #a4acaf;
-            }
-            &.electric{
-                background-color:#eed535;
-            }
-            &.ground{
-                background: linear-gradient(180deg, #f7de3f 50%, #ab9842 50%);
-            }
-            &.fairy{
-                background-color: #fdb9e9;
-            }
-            &.poison{
-                background-color: #A040A0;
-            }
-            &.fighting{
-                background-color: #d56723;
-            }
-            &.psychic{
-                background-color: #f366b9;
-            }
-            &.rock{
-                background-color: #a38c21;
-            }
-            &.steel{
-                background-color: #9eb7b8;
-            }
-            &.ice{
-                background-color: #51c4e7;
-            }
-            &.dragon{
-                background: linear-gradient(180deg, #53a4cf 50%, #f16e57 50%);
-            }
-            &.dark{
-                background-color: #707070;
-            }
-
-        }
-    } 
-`
+  }
+`;
